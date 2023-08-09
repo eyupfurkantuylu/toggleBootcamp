e595adada5700757f16ee5bf52d8d6c088ec148b
Eyüp Furkan Tüylü <eyupfurkantuylu@gmail.com>
2023-08-09 17:47:24 +0300

    Initial Commit

---
+++ toggleBootcamp/toggleBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  toggleBootcampApp.swift
+//  toggleBootcamp
+//
+//  Created by Eyüp Tüylü on 9.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct toggleBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
