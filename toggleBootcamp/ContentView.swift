8d325713e2a0b9944f6d84edbabac83ea46367e4
Eyüp Furkan Tüylü <eyupfurkantuylu@gmail.com>
2023-08-09 17:54:16 +0300

    ToggleBootcamp project

--- toggleBootcamp/ContentView.swift
+++ toggleBootcamp/ContentView.swift
@@ -8,12 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isOnline: Bool = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack{
+                Text("Status:").bold()
+                Text(isOnline ? "Online" : "Offline" ).bold()
+            }
+            .font(.title)
+            
+            Toggle(isOn: $isOnline) {
+                Text("Change status:")
+            }
+            
+            Spacer()
+            
         }
         .padding()
     }
